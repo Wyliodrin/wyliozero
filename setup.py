e1360bac3f5b7166225c9a965d565a3b812d8b5a
Alexandru Radovici <msg4alex@gmail.com>
2019-11-24 16:10:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wyliozero",
-    version="0.0.26",
+    version="0.0.27",
     author="Serban Razvan",
     author_email="razvan.serban@wyliodrin.com",
     description="Library for Wyliodrin Lab",
