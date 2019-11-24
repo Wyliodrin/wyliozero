66e3fe58f2b9fb2ab0e460d99a15865075e60512
Alexandru Radovici <msg4alex@gmail.com>
2019-11-24 15:27:57 +0200

    req

--- setup.py
+++ setup.py
@@ -13,12 +13,14 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Wyliodrin/wyliozero",
     packages=setuptools.find_packages(),
+    install_requires= ['gpiozero', 'Adafruit_DHT', 'pymata_aio', 'RPi.GPIO', 'Adafruit_CharLCD'],
     classifiers=[
         "Intended Audience :: Education",
         "Intended Audience :: Information Technology",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: BSD License",
         "Operating System :: Unix",
         "Topic :: Education"
     ],
+    python_requires='>=3.6'
 )
